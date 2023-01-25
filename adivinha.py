595b7d3cecdb0202246014a28fa1bcf75238662d
Caio Ross <49957185+caioross@users.noreply.github.com>
2023-01-25 01:20:15 -0300

    Create adivinha.py

---
+++ adivinha.py
@@ -0,0 +1,32 @@
+from random import randint
+
+print('#### Iníciando Jogo ####')
+
+random = randint(0, 100)
+chute = 0;
+chances = 10;
+while chute != random:
+    chute = input('Chute um número entre 0 a 100: ')
+    if chute.isnumeric():
+        chute = int(chute)
+        chances = chances - 1
+        if chute == random:
+            print('')
+            print('Parabéns, você venceu! O número era {} e você ainda tinha {} chances.'.format(random, chances))
+            print('')
+            break;
+        else:
+            print('')
+            if chute > random:
+                print('Você errou!!! Dica: É um número menor.')
+            else:
+                print('Você errou!!! Dica: É um número maior.')
+            print('Você ainda possui {} chances.'.format(chances))
+            print('')
+        if chances == 0:
+            print('')
+            print('Suas chances acabaram, você perdeu!')
+            print('')
+            break;
+
+print('#### Fim do Jogo ####')
